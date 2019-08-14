da180e6d278c5f004522d443d8a0a96f1c3d9003
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2019-08-14 19:48:25 +0530

    first commit tharinda

---
+++ src/test/java/com/solution/hangouts/HangoutsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solution.hangouts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HangoutsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
