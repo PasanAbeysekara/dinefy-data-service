f61888cadc4e3f32d31e801bc1acd18e057d44fd
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2020-04-30 00:51:42 +0530

    Sys Facility create , update and delete

--- src/main/java/com/solution/x/controller/sys/SysFacilityController.java
+++ src/main/java/com/solution/x/controller/sys/SysFacilityController.java
@@ -12,9 +12,11 @@ import org.springframework.hateoas.Link;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -79,30 +81,91 @@ public class SysFacilityController extends AbstractController<Facilities>
 	/**
 	 * Create a Facility
 	 *
-	 * @param facilities The Facility
+	 * @param facility The Facility
 	 * @return Saved Facility response
 	 */
 	@PostMapping("/facilities")
-	public ResponseEntity<ResponseWrapper<Facilities>> createFacility( @RequestBody Facilities facilities )
+	public ResponseEntity<ResponseWrapper<Facilities>> createFacility( @RequestBody Facilities facility )
 	{
 		ResponseEntity<ResponseWrapper<Facilities>> response;
 
 		try
 		{
-			Facilities savedFacility;
-			savedFacility = facilitiesRepository.save( facilities );
+			Facilities savedFacility = facilitiesRepository.save( facility );
 
 			Link selfRel = HATEOASProvider.sysFacilitySelfLinkProvider( savedFacility.getFacility_id() );
 			savedFacility.add( selfRel );
 
 			response = ResponseEntity.status( HttpStatus.CREATED )
 					.headers( addCommonHeaders( new HttpHeaders() ) )
-					.body( new ResponseWrapper<>( "OK", SystemMessages.FACILITY_CREATE_SUCCESS.getReasonPhrase(), savedFacility ) );
+					.body( new ResponseWrapper<>( "CREATED", SystemMessages.FACILITY_CREATE_SUCCESS.getReasonPhrase(), savedFacility ) );
 		}
 		catch( Exception e )
 		{
 			e.printStackTrace();
 			response = buildErrorResponse( SystemMessages.FACILITY_CREATE_FAILED, e );
+		}
+
+		return response;
+	}
+
+
+	/**
+	 * Update a Facility
+	 *
+	 * @param id       The Facility ID
+	 * @param facility The Facility
+	 * @return Updated Facility response
+	 */
+	@PutMapping("/facilities/{id}")
+	public ResponseEntity<ResponseWrapper<Facilities>> updateFacility( @PathVariable("id") int id, @RequestBody Facilities facility )
+	{
+		ResponseEntity<ResponseWrapper<Facilities>> response;
+
+		try
+		{
+			facility.setFacility_id( id );
+			Facilities savedFacility = facilitiesRepository.save( facility );
+
+			Link selfRel = HATEOASProvider.sysFacilitySelfLinkProvider( savedFacility.getFacility_id() );
+			savedFacility.add( selfRel );
+
+			response = ResponseEntity.ok()
+					.headers( addCommonHeaders( new HttpHeaders() ) )
+					.body( new ResponseWrapper<>( "UPDATED", SystemMessages.FACILITY_UPDATE_SUCCESS.getReasonPhrase(), savedFacility ) );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			response = buildErrorResponse( SystemMessages.FACILITY_UPDATE_FAILED, e );
+		}
+
+		return response;
+	}
+
+	/**
+	 * Delete a Facility
+	 *
+	 * @param id The Facility ID
+	 * @return Delete response
+	 */
+	@DeleteMapping("/facilities/{id}")
+	public ResponseEntity<ResponseWrapper<Facilities>> updateFacility( @PathVariable("id") int id )
+	{
+		ResponseEntity<ResponseWrapper<Facilities>> response;
+
+		try
+		{
+			facilitiesRepository.deleteById( id );
+
+			response = ResponseEntity.ok()
+					.headers( addCommonHeaders( new HttpHeaders() ) )
+					.body( new ResponseWrapper<>( "DELETED", SystemMessages.FACILITY_DELETE_SUCCESS.getReasonPhrase(), null ) );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			response = buildErrorResponse( SystemMessages.FACILITY_DELETE_FAILED, e );
 		}
 
 		return response;
--- src/main/java/com/solution/x/facade/SystemMessages.java
+++ src/main/java/com/solution/x/facade/SystemMessages.java
@@ -9,10 +9,12 @@ package com.solution.x.facade;
 public enum SystemMessages
 {
 
-	FACILITY_CREATE_SUCCESS( "100", "Facility successfully saved" ),
-	FACILITY_CREATE_FAILED( "101", "Facility saving failed" ),
-	FACILITY_UPDATE_FAILED( "102", "Facility update failed" ),
-	FACILITY_DELETE_FAILED( "103", "Facility delete failed" );
+	FACILITY_CREATE_SUCCESS( "1100", "Facility successfully saved" ),
+	FACILITY_CREATE_FAILED( "1101", "Facility saving failed" ),
+	FACILITY_UPDATE_SUCCESS( "1102", "Facility successfully updated" ),
+	FACILITY_UPDATE_FAILED( "1103", "Facility update failed" ),
+	FACILITY_DELETE_SUCCESS( "1104", "Facility successfully deleted" ),
+	FACILITY_DELETE_FAILED( "1105", "Facility delete failed" );
 
 	private final String code;
 	private final String reasonPhrase;
