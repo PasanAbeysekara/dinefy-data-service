64f1ee56c7eb52351a6465057820661d197e526f
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2020-05-23 10:10:10 +0530

    Prop Delete

--- src/main/java/com/solution/x/controller/PropertyController.java
+++ src/main/java/com/solution/x/controller/PropertyController.java
@@ -6,6 +6,7 @@ import com.solution.x.dao.Property;
 import com.solution.x.facade.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,6 +75,12 @@ public class PropertyController
 	public ResponseEntity<ResponseWrapper<Property>> updateProperty( @PathVariable("id") long id, @RequestBody Property property )
 	{
 		return propertyService.updateProperty( id, property );
+	}
+
+	@DeleteMapping("/properties/{id}")
+	public ResponseEntity<ResponseWrapper<Property>> deleteProperty( @PathVariable("id") long id )
+	{
+		return propertyService.deleteProperty( id );
 	}
 
 	/**
