a7ca9bea5c92f9fa541ef5d9b40a01456a906dd3
Pasan Abeysekara <pasankavindaabey@gmail.com>
2024-04-02 08:49:28 +0530

    FIX BUG: mismatched datatypes

--- src/main/java/com/thaprobit/resengine/ReservationEngineData.java
+++ src/main/java/com/thaprobit/resengine/ReservationEngineData.java
@@ -15,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         "com.thaprobit.resengine.messaging",
         "com.thaprobit.resengine.config"
 })
+
 @EnableScheduling
 @EnableAsync
 public class ReservationEngineData {
